0374ab87b7ddf2a5a8e8a507181a72dda957cb40
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-16 12:08:12 -0300

    Initial Commit

---
+++ appetizers/appetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  appetizersApp.swift
+//  appetizers
+//
+//  Created by Agustin Carbajal on 16/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct appetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
