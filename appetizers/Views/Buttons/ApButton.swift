9d7ceaef43e2a3a78fa2d58bb0f6fcbe504db4b2
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-16 19:14:22 -0300

    [chore] refactoring

---
+++ appetizers/Views/Buttons/ApButton.swift
@@ -0,0 +1,20 @@
+//
+//  ApButton.swift
+//  appetizers
+//
+//  Created by Agustin Carbajal on 16/11/2023.
+//
+
+import SwiftUI
+
+struct ApButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ApButton_Previews: PreviewProvider {
+    static var previews: some View {
+        ApButton()
+    }
+}
