f5e3dd51e034a0a6591805840ecaf3bf00597146
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-16 16:01:00 -0300

    [feature] error handling

--- appetizers/Alert.swift
+++ appetizers/Alert.swift
@@ -1,8 +1,53 @@
 //
 //  Alert.swift
-//  appetizers
+//  SwiftUI_Appetizers
 //
-//  Created by Agustin Carbajal on 16/11/2023.
+//  Created by Alex 6.1 on 4/19/21.
 //
 
-import Foundation
+import SwiftUI
+
+struct AlertItem: Identifiable {
+    let id = UUID()
+    let title: Text
+    let message: Text
+    let dismissButton: Alert.Button
+}
+
+struct AlertContext {
+    
+    //MARK: - NETWORK ALERTS
+    static let invalidData      = AlertItem(title: Text("Server Error"),
+                                              message: Text("The data recieved from the server was invalid. Contact Support."),
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidResponse  = AlertItem(title: Text("Server Error"),
+                                              message: Text("Invalid response from the server. Please try again."),
+                                              dismissButton: .default(Text("OK")))
+
+    static let invalidURL       = AlertItem(title: Text("Server Error"),
+                                              message: Text("There was an issue connecting to the server. If this persists contact Support."),
+                                              dismissButton: .default(Text("OK")))
+    
+    static let unableToComplete = AlertItem(title: Text("Server Error"),
+                                              message: Text("Unable to complete your request please check internet connection."),
+                                              dismissButton: .default(Text("OK")))
+
+    
+    //MARK: -- ACOUNT ALERTS
+    static let invalidForm      = AlertItem(title: Text("Invalid Form"),
+                                              message: Text("Please ensure all fields are completed."),
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidEmail     = AlertItem(title: Text("Invalid Email"),
+                                              message: Text("Please ensure your email is correct."),
+                                              dismissButton: .default(Text("OK")))
+
+    static let userSaveSuccess  = AlertItem(title: Text("Profile Saved"),
+                                              message: Text("Your profile information was successfully saved."),
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidUserData  = AlertItem(title: Text("Profile Error"),
+                                              message: Text("There was an error saving or retrieving your profile."),
+                                              dismissButton: .default(Text("OK")))
+}
--- appetizers/AppetizersListView.swift
+++ appetizers/AppetizersListView.swift
@@ -15,6 +15,9 @@ struct AppetizersListView: View {
         .onAppear{
             viewModel.getAppetizers()
         }
+        .alert(item: $viewModel.alertItem){ alertItem in
+            Alert(title: alertItem.title, message: alertItem.message, dismissButton: alertItem.dismissButton)
+        }
     }
 }
 
